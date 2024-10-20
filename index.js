32a27f62c6345e53997368bc0e137edc93b6608a
VincentKho10 <38545087+VincentKho10@users.noreply.github.com>
2024-10-20 14:57:53 +0700

    unbreakable calculator
    next adding decimal

--- index.js
+++ index.js
@@ -9,17 +9,8 @@ let isInit = true;
 let operator = "";
 let isDotUsed = false;
 
-const numbernodes = ["0123456789."];
+const numbernodes = ["0123456789"];
 const operatornodes = ["*/+-="];
-
-const enableDotButton = (isOn) => {
-  const dot = document.querySelector("#btndot");
-  if (isOn) {
-    dot.removeAttribute("disabled");
-  } else {
-    dot.setAttribute("disabled", "");
-  }
-};
 
 const resetOperand = () => (operand = "");
 const varReset = () => {
@@ -27,13 +18,25 @@ const varReset = () => {
   result = "";
   isInit = true;
   operator = "";
-  isDotUsed = false;
 };
+
+const displayCalculation = (isCalculation, charoperator)=>{
+  // console.log(`operator: ${operator}, operand: ${operand}, result:${result}`)
+  if (isCalculation){
+    if (charoperator == "=") {
+      console.log("end result:" + parseFloat(result));
+    } else {
+      operator = charoperator;
+      console.log("current state:" + parseFloat(result) + " " + operator);
+    }
+  }else{
+    console.log("operand: " + operand);
+  }
+}
 
 const calculationProcess = (char) => {
   result = parseFloat(result);
   operand = parseFloat(operand);
-  operator = char;
   switch (operator) {
     case "+":
       result += operand;
@@ -50,37 +53,21 @@ const calculationProcess = (char) => {
     default:
       break;
   }
-  if (char == "=") {
-    console.log("end result:" + parseFloat(result));
-  } else{
-    console.log("current state:" + parseFloat(result) + " " + operator);
-  }
-  isDotUsed = false;
 };
 
 [...numbernodes[0]].forEach((char, idx) => {
   const btnnumber = document.createElement("button");
   btnnumber.textContent = char;
-  if (char == ".") {
-    btnnumber.setAttribute("disabled", "")
-    btnnumber.setAttribute('id', 'btndot')
-  };
+  // if (char == ".") {
+  //   btnnumber.setAttribute("id", "btndot");
+  // }
   btnnumber.addEventListener("click", () => {
+    if (operator=="="){
+      result=""
+      isInit = true
+    }
     operand += char;
-    console.log("operand: " + operand);
-
-    if(operand.length>=1 && !isDotUsed){
-      enableDotButton(true)
-    }
-
-    if(char=="."){
-      enableDotButton(false)
-      isDotUsed=true
-    }
-
-    if (isInit) {
-      result = operand;
-    }
+    displayCalculation(false);
   });
   numbernodes.push(btnnumber);
 });
@@ -90,11 +77,18 @@ numbernodes.splice(0, 1);
   const btnoperator = document.createElement("button");
   btnoperator.textContent = char;
   btnoperator.addEventListener("click", () => {
-    calculationProcess(char);
-    if (char == "=") {
-      varReset();
-      return;
+    if(isInit){
+      result = operand
+      operand = ""
+      operator = char
+      isInit = false
+      return
     }
+    if(operand!=""&&result!=""){
+      calculationProcess(char);
+    }
+    operator = char
+    displayCalculation(true, char)
     resetOperand();
   });
   operatornodes.push(btnoperator);
